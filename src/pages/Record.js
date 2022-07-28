6b3690901bc8b65678dc0fae22bb86ab6c5f1650
Abner <xiaocao1602@qq.com>
2022-07-28 22:49:41 +0800

    feat: 实现 rrweb 录制&播放能力

--- src/pages/Record.js
+++ src/pages/Record.js
@@ -1,9 +1,68 @@
-import React from 'react';
+// import { record } from 'rrweb';
+// import rrwebPlayer from 'rrweb-player';
+import record from 'rrweb/es/rrweb/packages/rrweb/src/record/index';
+import { Replayer } from 'rrweb/es/rrweb/packages/rrweb/src/replay/index';
+import 'rrweb-player/dist/style.css';
+import logo from '../logo.svg';
 
-export default function Record () {
+
+const events = [];
+let stopFn;
+let player;
+
+export default function Record() {
+  const handleRecord = () => {
+    stopFn = record({
+      emit(event) {
+        console.log('rrweb', event);
+        events.push(event);
+      },
+    });
+  };
+
+  const handleStop = () => {
+    stopFn();
+    handlePlayer();
+  }
+
+  const handlePlayer = () => {
+    player = new Replayer(events, {
+      root: document.getElementById('testAa')
+    });
+  }
+
   return (
-    <div>
-      <div>record</div>
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+        <input className='rr-mask' style={{ display: 'inline-block', margin: '20px 0' }} type="text" />
+
+        <button onClick={handleRecord}>录制</button>
+        <button onClick={handleStop} style={{ display: 'inline-block', margin: '20px 0' }}>停止</button>
+
+      </div>
+
+      <div className='player-wrap'>
+        <div id="testAa" />
+        <div className='operation'>
+          <button onClick={() => {
+            debugger
+            player.play()
+          }}>播放</button>
+        </div>
+      </div>
     </div>
-  )
-};
+  );
+}
